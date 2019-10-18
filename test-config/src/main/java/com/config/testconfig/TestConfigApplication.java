f7f9eaf222497e8924bdd1a72766c9613cdd0369
lipenggang@touscm.com <lipenggang@touscm.com>
2019-10-18 11:44:47 +0800

    搜索

--- test-config/src/main/java/com/config/testconfig/TestConfigApplication.java
+++ test-config/src/main/java/com/config/testconfig/TestConfigApplication.java
@@ -2,12 +2,12 @@ package com.config.testconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootConfiguration
-@EnableAutoConfiguration
+@SpringBootApplication
 public class TestConfigApplication {
 
     public static void main(String[] args) {
